0871cabdac2bff933f70bc6a4098de2fa8f7acf8
RIL75 <44982539+RIL75@users.noreply.github.com>
2023-02-27 10:42:22 +0100

    Update Übung.sql

--- Oracle/oracle-sample-database/Übung.sql
+++ Oracle/oracle-sample-database/Übung.sql
@@ -1,4 +1,48 @@
 select table_name from user_tables ORDER BY table_name;
 
 
+select count(*) from ot.contacts;
 
+select name from ot.customers;
+
+
+SELECT
+    customer_id,
+    name,
+    address,
+    website,
+    credit_limit
+FROM
+    ot.customers;
+    
+    
+    SELECT
+    name,
+    address,
+    website,
+    credit_limit
+FROM
+    ot.customers;
+    
+    
+    
+    
+    SELECT
+    name,
+    credit_limit
+FROM
+    ot.customers
+ORDER BY
+    2 DESC,
+    1;
+    
+    
+    
+    SELECT
+    country_id,
+    city,
+    state
+FROM
+    ot.locations
+ORDER BY
+    state ASC NULLS FIRST;
