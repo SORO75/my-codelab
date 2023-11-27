5a1596dcacae26748aec1dfd02ed7fc9eda884b7
RIL75 <44982539+RIL75@users.noreply.github.com>
2023-11-27 13:52:37 +0100

    Update 096-104.py

--- Python/150_challenges/096-104.py
+++ Python/150_challenges/096-104.py
@@ -4,13 +4,14 @@ Create the following using a simple 2D list using the standard Python indexing:
 3   7   4
 1   6   9
 4   2   0
-
 '''
+
 
 '''097
 Using the 2D list from program 096, 
 ask the user to select a row and a column and display that value.
 '''
+
 
 '''098
 Using the 2D list from program 096, 
