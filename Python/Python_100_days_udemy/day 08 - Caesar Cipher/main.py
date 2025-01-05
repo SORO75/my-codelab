79dec86cfda854cb5f943df181e73c6d31443b6e
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-01-05 12:03:55 +0100

    day8 Update

--- Python/Python_100_days_udemy/day 08 - Caesar Cipher/main.py
+++ Python/Python_100_days_udemy/day 08 - Caesar Cipher/main.py
@@ -1,6 +1,8 @@
 alphabet_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                   'u', 'v', 'w', 'x', 'y', 'z']
 
+print(len(alphabet_lower))
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number\n"))
@@ -8,12 +10,48 @@ shift = int(input("Type the shift number\n"))
 
 # TODO-1: create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs
 
+
+
+
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift mount and print the encrypted text
 
-
 def encrypt(original_text, shift_amount):
-    
+    output_text = ""
+    for l in original_text:
+       # print(alphabet_lower.index(l))
+      #  if enc_dec == "decode":
+       #    shift_amount *= -1
+
+        shifted_position = alphabet_lower.index(l)+shift_amount
+        shifted_position %= len(alphabet_lower)
+        output_text+=alphabet_lower[shifted_position]
+    print(f"Here is the encoded result: {output_text}")
+
+
+def decrypt(original_text, shift_amount):
+    output_text = ""
+    for l in original_text:
+        shifted_position = alphabet_lower.index(l) - shift_amount
+        shifted_position %= len(alphabet_lower)
+        output_text += alphabet_lower[shifted_position]
+    print(f"Here is the encoded result: {output_text}")
+
+def caesar(original_text=text, shift_amount=shift, enc_dec=direction):
+    output_text = ""
+    if enc_dec =='decode':
+        shift_amount *= -1
+    for l in original_text:
+        shifted_position = alphabet_lower.index(l) + shift_amount
+        shifted_position %= len(alphabet_lower)
+        output_text += alphabet_lower[shifted_position]
+    print(f"Here is the encoded result: {output_text}")
+
+
+
+encrypt(original_text=text, shift_amount=shift, enc_dec=direction)
+
+#print(alphabet_lower[0])
 
 
 # TODO-3: Call the 'encrypt()' function and  pass in the user inputs.
