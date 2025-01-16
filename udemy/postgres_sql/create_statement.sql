f2ce9db1377215c9b9825faaa9ae0f1f9eff4e26
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-01-16 12:11:09 +0100

    day 2

--- udemy/postgres_sql/create_statement.sql
+++ udemy/postgres_sql/create_statement.sql
@@ -35,4 +35,29 @@ FROM
   cities;
 
 
-  
+
+
+
+  SELECT name, area FROM cities WHERE area > 4000;
+
+SELECT name, price FROM phones
+WHERE units_sold > 5000
+
+SELECT name, manufacturer FROM phones
+WHERE manufacturer IN ('Apple','Samsung')
+
+SELECT name,(price*units_sold) AS total_revenue FROM phones
+WHERE (price*units_sold) > 1000000
+
+-- Write query here to update the 'units_sold' of the phone with name 'N8' to 8543
+UPDATE phones
+SET units_sold = 8543
+WHERE name = 'N8';
+-- Write query here to select all rows and columns of the 'phones' table
+SELECT * FROM phones;
+
+-- Write your delete SQL here
+DELETE FROM phones
+WHERE manufacturer IN ('Samsung');
+
+-- Write query here to select all rows and columns from phones
