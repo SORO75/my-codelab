5c1a67df6d3d8bef1ac495781ac2ee12cf3a7830
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-01-23 17:52:53 +0100

    Update delete commands

--- udemy/postgres_sql/photogalerie.sql
+++ udemy/postgres_sql/photogalerie.sql
@@ -10,4 +10,63 @@ VALUES
   ('si93onis'),
   ('99stroman');
 
-SELECT * FROM users;
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id)
+);
+
+INSERT INTO photos (url, user_id)
+VALUES
+	('http://one.jpg', 4);
+
+INSERT INTO photos (url, user_id)
+VALUES
+	('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://754.jpg', 2),
+  ('http://35.jpg', 3),
+  ('http://256.jpg', 4);
+
+INSERT INTO photos (url, user_id)
+VALUES ('http://jpg', NULL);
+
+DROP TABLE photos;
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO photos (url, user_id)
+VALUES
+	('http://one.jpg', 4),
+	('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://754.jpg', 2),
+  ('http://35.jpg', 3),
+  ('http://256.jpg', 4);
+
+DELETE FROM users
+WHERE id = 1;
+
+DROP TABLE photos;
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id) ON DELETE SET NULL
+);
+
+INSERT INTO photos (url, user_id)
+VALUES
+  ('http:/one.jpg', 4),
+  ('http:/754.jpg', 2),
+  ('http:/35.jpg', 3),
+  ('http:/256.jpg', 4);
+
+DELETE FROM users
+WHERE id = 4;
