03e8b9b1c0acce27e6bf0fc1d928b12c09c1da92
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-06-26 20:01:36 +0200

    sql_tocsv_excel

--- udemy/ automate_all_with_python/SQL_DBs/sql_to_csv_xls.py
+++ udemy/ automate_all_with_python/SQL_DBs/sql_to_csv_xls.py
@@ -1,8 +1,12 @@
 import sqlite3
+import pandas as pd
 
 #Establish a connection and create a cursor
 con = sqlite3.connect('database.db')
 cur = con.cursor()
 
-cur.execute("SELECT * FROM 'ips' ORDER BY asn")
-print(cur.fetchall())
+df =pd.read_sql_query("SELECT * FROM 'ips' ORDER BY asn", con)
+print(df)
+
+df.to_csv('database.csv', index=None)
+df.to_excel('database.xlsx', index=None)
