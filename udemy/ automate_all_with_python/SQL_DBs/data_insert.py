ab23383bc4ac6707bf92744389c30e4e971d9a7c
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-06-27 07:54:18 +0200

    Data_insert_SQL

---
+++ udemy/ automate_all_with_python/SQL_DBs/data_insert.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+#Establish a connection and create a cursor
+con = sqlite3.connect('database.db')
+cur = con.cursor()
+
+
+new_rows = [
+('100.100.100.100', 'a,b,c', 100),
+('200.200.200.200', 'f.d.e', 200)
+]
+
+cur.executemany("INSERT INTO 'ips' VALUES(?,?,?)", new_rows)
+con.commit()
+
+cur.execute("SELECT * FROM 'ips' ORDER BY asn")
+print(cur.fetchall())
