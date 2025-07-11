07612731002493849c90f5abd05e3a6c1e3f6cf9
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-07-11 17:23:28 +0200

    update

--- udemy/ automate_all_with_python/Files_and_folder/main.py
+++ udemy/ automate_all_with_python/Files_and_folder/main.py
@@ -20,3 +20,26 @@ print(list[p2.iterdir()])
 
 
 # add prefix to all filenames in folder
+
+root_dir = Path('files')
+file_paths = root_dir.iterdir()
+print(Path.cwd())
+
+for path in file_paths:
+    new_filename =  "new-" + path.stem +path.suffix
+    new_filepath = path.with_name(new_filename)
+    print(new_filepath)
+    path.rename(new_filepath)
+
+# rename all files based on folder
+
+file_paths = root_dir.glob("**/*")
+
+for path in file_paths:
+    if path.is_file():
+        parent_folder = path.parts[-2]
+        new_filename = parent_folder + '-'+ path.name
+        path.rename(new_filepath)
+        print(parent_folder)
+
+
