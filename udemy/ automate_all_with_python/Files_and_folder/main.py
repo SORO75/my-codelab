fe0f6bf8deac5ea37a3cb4f6cd94b918d92b8b2b
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-07-15 15:58:01 +0200

    Update main.py

--- udemy/ automate_all_with_python/Files_and_folder/main.py
+++ udemy/ automate_all_with_python/Files_and_folder/main.py
@@ -90,6 +90,15 @@ for path in root_dir.glob("*.zip"):
         zf.extactall(path= final_path)
 
 #seach file in computer
+root_dir = Path('.')
+search_term = '14'
+
+for path in root_dir.rglob("*"):
+    if path.is_file():
+        if search_term in path.stem:
+            print(path.absolute())
+
+
 # destroy files forever
 
 
