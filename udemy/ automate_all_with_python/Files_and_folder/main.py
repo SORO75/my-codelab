5765fa02880f3bb2728413dfff6f6c87ef09eefa
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-07-15 15:16:43 +0200

    Update main.py

--- udemy/ automate_all_with_python/Files_and_folder/main.py
+++ udemy/ automate_all_with_python/Files_and_folder/main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from datetime import datetime
+
 p1 = Path('files/abc.txt')
 print(type(p1))
 
@@ -42,4 +44,16 @@ for path in file_paths:
         path.rename(new_filepath)
         print(parent_folder)
 
+
+#add created date to all filenames in folder
+
+root_dir = Path('files')
+
+for path in root_dir.glob("**/*"):
+  if path.is_file():
+    created_date = datetime.fromtimestamp(path.stat().st_ctime)
+    created_date_str = created_date.strftime("%Y-%m-%d_%H:%M:%S")
+    new_filename = created_date_str + '_' + path.name
+    new_filepath = path.with_name(new_filename)
+    path.rename(new_filepath)
 
