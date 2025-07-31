0ad4496dca7329efd506bd7f8201147f86bc426b
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2025-07-31 17:23:41 +0200

    email_automate update

--- udemy/ automate_all_with_python/Automating_Emails/send_single_email.py
+++ udemy/ automate_all_with_python/Automating_Emails/send_single_email.py
@@ -1,8 +1,10 @@
 import yagmail
 import os
+import time
+from datetime import datetime as dt
 
-sender = 'app7flask@gmail.com'
-receiver = '2jjnkjca@10mail.tk'
+sender = 'jflsjsljsk@gmail.com'
+receiver = '2lslfdm10@gmail.com'
 
 subject = "This is the subject!"
 
@@ -11,7 +13,10 @@ contents = """
 Here is the content of the email! 
 Hi!
 """
-
-yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
-yag.send(to=receiver, subject=subject, contents=contents)
-print("Email Sent!")
+while True:
+    now = dt.now()
+    if dt.now().hour == 13 and now.minute == 15:
+        yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
+        yag.send(to=receiver, subject=subject, contents=contents)
+        print("Email Sent!")
+        time.sleep(60)
