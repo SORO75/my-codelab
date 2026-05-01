15dda61c73be216607aa4f73e3ae1353886b0c73
S0R0 ソロ <r.sorokun@pm.me>
2026-05-01 13:01:30 +0200

    sqlite3 initial

---
+++ python/dbs/sqlite/connect.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+try:
+    with sqlite3.connect('test.db') as conn:
+        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+
+except sqlite3.OperationalError as err:
+    print("Failed to open databse:", err)
+
+
+    
+
+
+
+
