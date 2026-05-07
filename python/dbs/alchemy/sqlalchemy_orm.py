7ceea520c27fa790fba7df6986a308621f10ce95
S0R0 ソロ <r.sorokun@pm.me>
2026-05-07 17:08:46 +0200

    data engineering project structure change

---
+++ python/dbs/alchemy/sqlalchemy_orm.py
@@ -0,0 +1,71 @@
+'''
+Basis model definition
+CRUD Operation with ORM
+Querying with ORM
+limiting Results
+subqueries
+One-to-Many Relationships
+Many-to-Many Relationships
+inheritnace Mapping
+
+Joined Table inheritance
+Eageer Loading
+LazyLoading
+Joins Optimisation
+Query cashing
+query execution plan
+bulk operation
+Proper indexing
+connection pooling
+profiling queries
+Session management
+Transactions management
+concurrence and isolation levels
+
+migration with Alembic
+
+asynchron I/O
+CRUD with Async
+
+Best practices for Async
+
+Testing with SQLalchemy
+    Units Tests
+    Transactions Tests
+    Integration Tests
+Mocking  Database Calls
+FastAPI Integration
+FastAPI with SQLAlchemy Async
+Pydantic Integration
+Design Pattern best Practices
+    Unit of Work Pattern
+    Data Mapper Pattern
+    Query Object Pattern
+    Lazy Loading Pattern
+Perfomance Monitoring and Optimization
+Profiling  Database Calls
+    Unit of Work Pattern
+    using SQL Echo
+    Time Logging
+    Profiling Extension
+    N+1 Query Problem
+Connection Pooling and Management
+Monitoring
+Security Best Practices
+Preventing SQL Injection
+Authentication and Authorization
+Application Scaling
+    Vertical
+    Horizontal
+    Load Balancing
+    Sharding
+    ReadReplicas
+    Cashing
+    RestAPI with SQLAlchemy
+    Simpel Bog engine
+    Data Analysis APplication
+Debugging
+    Debug Logging
+Handling Connections Fehler
+
+'''
