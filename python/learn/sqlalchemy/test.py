f976de6eff1ae25522b237e49fa24dfe14d869cd
S0R0 ソロ <r.sorokun@pm.me>
2026-05-28 18:46:55 +0200

    sqlalchemy advantage

---
+++ python/learn/sqlalchemy/test.py
@@ -0,0 +1,86 @@
+from sqlalchemy import Column, Integer, String, create_engine, MetaData, Table, insert
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import SQLAlchemyError  
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+#one-to-many relationship example
+
+class Parent (Base):
+    __tablename__ = 'parent'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    children = relationship("Child", back_populates="parent")
+
+class Child (Base):
+    __tablename__ = 'child'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    parent_id = Column(Integer, ForeignKey('parent.id'))
+    parent = relationship("Parent", back_populates="children")
+
+
+    # Create a parent with two children
+parent = Parent(name='Parent Name')
+child1 = Child(name='Child 1', parent=parent)
+child2 = Child(name='Child 2', parent=parent)   
+parent.children = [child1, child2]
+
+
+session.add(parent)
+session.commit()
+
+#Query the parent and its children
+queried_parent = session.query(Parent).filter_by(name='Parent Name').first()
+print(f"Parent: {queried_parent.name}")
+for child in queried_parent.children:
+    print(f"Child: {child.name}")
+
+
+#Many-to-many relationship example
+
+#Associatiion table
+student_course = Table('student_course', Base.metadata,
+    Column('student_id', Integer, ForeignKey('student.id')),
+    Column('course_id', Integer, ForeignKey('course.id'))
+)
+
+class Student(Base):
+    __tablename__ = 'student'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    courses = relationship("Course", secondary=student_course, back_populates="students")   
+
+
+class Course(Base):
+    __tablename__ = 'course'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    students = relationship("Student", secondary=student_course, back_populates="courses")  
+
+
+# Create a student and two courses
+student1 = Student(name='Student Name1')
+student2 = Student(name='Student Name2')
+course1 = Course(name='Course 1')
+course2 = Course(name='Course 2')
+
+enroll students in courses
+student1.courses = [course1, course2]
+student2.courses = [course1]
+
+session.add_all([student1, student2, course1, course2])
+session.commit()
+
+#Query the student and their courses
+queried_student = session.query(Student).filter_by(name='Student Name1').first()
+print(f"Student: {queried_student.name}")
+for course in queried_student.courses:
+    print(f"Course: {course.name}")
+
+
+# Inheritance example
+
+c
