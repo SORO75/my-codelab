4074301264a6d9568c6305056c70ee5745b79989
S0R0 ソロ <r.sorokun@pm.me>
2026-06-10 11:15:59 +0200

    add CRUD functions with async sqlalchemy

--- python/learn/sqlalchemy/aqlalchemy_async.py
+++ python/learn/sqlalchemy/aqlalchemy_async.py
@@ -4,6 +4,11 @@ from sqlalchemy import select
 
 from learn.sqlalchemy.sqlalchemy_orm import User
 
+
+
+
+
+
 # Create async engine
 engine = create_async_engine('postgresql+asyncpg://postgres:1234@localhost:5432/postgres', echo=True)
 
@@ -22,6 +27,75 @@ import asyncio
 async def main():
     user = await get_user(1)
     print(user.name if user else "User not found")
+
+
+# CRUD Operations with Async SQLALlhemy
+
+# Create
+async  def create_user(name:str, email:str) -> User:
+    async with async_session() as session:
+        new_user = User(name=name, email=email)
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+        return new_user
+
+# Usage create
+async def get_user_by_email(email:str) -> User:
+
+    user = await create_user("user1", "user1@exmale.com")
+    print(f"Created user: {user.name}")
+
+asyncio.run(get_user_by_email(""))
+
+# Read
+async def get_user_by_id(user_id: int) -> User:
+    async with async_session() as session:
+        stmt = select(User).where(User.id == user_id)
+        result = await session.execute(stmt)
+        return result.scalar_one_or_none()
+
+# Usage read
+pass
+
+# Update
+async def update_user_email(user_id:int, new_email:str) -> User:
+    async with async_session() as session:
+        stmt = select(User).where(User.id == user_id)
+        result = await session.execute(stmt)
+        updated_user = result.scalar_one_or_none()
+        if updated_user:
+            updated_user.email = new_email
+            await session.commit()
+            return updated_user
+        return None
+#Usage update
+pass
+
+# Delete
+async def delete_user(user_id:int) -> User:
+    async with async_session() as session:
+        stmt = select(User).where(User.id == user_id)
+        result = await session.execute(stmt)
+        deleted_user = result.scalar_one_or_none()
+        if deleted_user:
+            await session.delete(deleted_user)
+            await session.commit()
+            return True
+        return False
+
+#Usage Delete
+pass
+
+
+
+
+
+
+
+
+
+
 
 asyncio.run(main())
 
